21f413e3c6346d4e5e3c5b8531265f116234c14f
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-09-28 16:21:48 +0100

    Added Assets and Player Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //designer variables
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis="Horizontal";
+    public string verticalAxis= "Vertical";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Get axis input from Unity
+        float leftRight=Input.GetAxis(horizontalAxis);
+        float upDown = Input.GetAxis(verticalAxis);
+
+        //Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight,upDown);
+
+        //Make direction vector length 1
+        direction = direction.normalized;
+
+        //calculate velocity
+        Vector2 velocity = direction * speed;
+
+        //give the velocity to the rigid body
+        physicsBody.velocity = velocity;
+
+	}
+}
