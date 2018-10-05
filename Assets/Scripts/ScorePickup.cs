7a1bbecd6d671ad830b97a9baaaab001ecea1f76
MHadley98 <43067353+MHadley98@users.noreply.github.com>
2018-10-05 14:32:44 +0100

    Added Items, Score and Tiles

---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        //Only do if it's hit the player
+        if(collision.GetComponent<Player>())
+        {
+            //Updating the score based on how much the score is worth
+            scoreTracker.ChangeValue(scoreValue);
+
+            //Gets rid of the item
+            Destroy(gameObject);
+      
+        }
+    }
+
+}
